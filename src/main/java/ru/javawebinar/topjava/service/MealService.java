0c3adc24403f0b200ed06be52fa1e3034335e982
AlekseyKovalenko1993 <aleksey1932@gmail.com>
2019-06-17 15:22:27 +0300

    Realise MealService

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,4 +1,20 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.to.MealTo;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.List;
+
 public interface MealService {
+    Meal create(Meal meal);
+
+    Meal update(Meal meal);
+
+    void delete(int id, int userId) throws NotFoundException;
+
+    List<MealTo> getAll(int userId,int caloriesPerDay);
+
+    Meal get(int id,int userId) throws NotFoundException;
+
 }
--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,9 +1,49 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.to.MealTo;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.util.ValidationUtil;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class MealServiceImpl implements MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    @Autowired
+    public MealServiceImpl(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Meal create(Meal meal) {
+        return repository.save(meal);
+    }
+
+    @Override
+    public Meal update(Meal meal) {
+        return ValidationUtil.checkNotFoundWithId(meal,meal.getId());
+    }
+
+    @Override
+    public void delete(int id, int userId) throws NotFoundException {
+        ValidationUtil.checkNotFoundWithId(repository.delete(id,userId),id);
+    }
+
+    @Override
+    public List<MealTo> getAll(int userId,int caloriesPerDay) {
+        return MealsUtil.getWithExcess(repository.getAll(userId),caloriesPerDay);
+    }
+
+    @Override
+    public Meal get(int id, int userId) throws NotFoundException {
+        return ValidationUtil.checkNotFoundWithId(repository.get(id,userId),id);
+    }
 }
