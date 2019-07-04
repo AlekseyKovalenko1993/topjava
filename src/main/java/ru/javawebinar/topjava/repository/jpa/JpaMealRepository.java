ee8d3cc3f440c6979609b3f854388e9e39e45359
AlekseyKovalenko1993 <aleksey1932@gmail.com>
2019-07-04 17:26:43 +0300

    Realize JPARepository

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
@@ -16,27 +17,46 @@ public class JpaMealRepository implements MealRepository {
     private EntityManager em;
 
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
-        return null;
+        if(meal.isNew()){
+            em.persist(meal);
+            return meal;
+        }else
+            return em.merge(meal);
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        return em.createQuery("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+                .setParameter("id", id)
+                .setParameter("userId", userId)
+                .executeUpdate() > 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return em.createQuery("SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=:userId",Meal.class)
+                .setParameter("id",id)
+                .setParameter("userId",userId)
+                .getSingleResult();
+
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return em.createQuery("SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC",Meal.class)
+                .setParameter("userId",userId)
+                .getResultList();
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return em.createQuery("SELECT m FROM Meal m WHERE m.dateTime>=:startDate AND m.dateTime<=:endDate AND m.user.id=:userId ORDER BY m.dateTime DESC ",Meal.class)
+                .setParameter("startDate",startDate)
+                .setParameter("endDate",endDate)
+                .setParameter("userId",userId)
+                .getResultList();
     }
 }
