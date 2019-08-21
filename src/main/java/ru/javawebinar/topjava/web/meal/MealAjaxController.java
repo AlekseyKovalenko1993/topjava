2f40d911f573d7d205542707aa8d9df5def1bf90
AlekseyKovalenko1993 <aleksey1932@gmail.com>
2019-08-21 11:52:11 +0300

    Add controller and datatable fro meals

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -0,0 +1,62 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.web.SecurityUtil;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ajax/profile/meals")
+public class MealAjaxController extends AbstractMealController {
+    static final String AJAX_URL = "/ajax/profile/meals";
+
+    @Override
+    @GetMapping("/{id}")
+    public Meal get(@PathVariable int id) {
+        return super.get(id);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
+
+    @Override
+    @GetMapping
+    public List<MealTo> getAll() {
+        return super.getAll();
+    }
+
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void createUpdate(@RequestParam int id,
+                             @RequestParam LocalDateTime dateTime,
+                             @RequestParam String description,
+                             @RequestParam int calories){
+        Meal meal = new Meal(id,dateTime,description,calories);
+        if(meal.isNew()){
+            super.create(meal);
+        }else{
+            super.update(meal, SecurityUtil.authUserId());
+        }
+    }
+
+
+    @Override
+    @GetMapping(value = "/filter")
+    public List<MealTo> getBetween(
+            @RequestParam(required = false) LocalDate startDate,
+            @RequestParam(required = false) LocalTime startTime,
+            @RequestParam(required = false) LocalDate endDate,
+            @RequestParam(required = false) LocalTime endTime) {
+        return super.getBetween(startDate, startTime, endDate, endTime);
+    }
+}
---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function () {
+    makeEditable({
+        ajaxUrl : "ajax/profile/meals/",
+        dataTable : $("#datatable").dataTable({
+            "paging" : false,
+            "info" : true,
+            "columns" : [
+                {
+                    "data" : "dateTime"
+                },
+                {
+                    "data" : "description"
+                },
+                {
+                    "data" : "calories"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "desc"
+                ]
+            ]
+        })
+    })
+});
